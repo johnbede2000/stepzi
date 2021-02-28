ffc3eeb2f59cb38c89d82010254533c2a270ab4c
John Bede <johnbede2000@gmail.com>
2021-02-28 21:06:34 +0000

    textfield on page one

--- src/components/InitialGoal.js
+++ src/components/InitialGoal.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const InitialGoal = () => {
   return (
     <div>
       <p>Write down your goal:</p>
+      <TextField
+        id="goal"
+        label="Goal"
+        type="text"
+        multiline="true"
+        variant="filled"
+        required="true"
+      />
     </div>
   );
 };
