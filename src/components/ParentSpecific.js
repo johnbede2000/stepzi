9805a4116b1792b7c207563466406eeb4c1864d8
John Bede <johnbede2000@gmail.com>
2021-02-28 15:18:47 +0000

    created components and state

---
+++ src/components/ParentSpecific.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ParentSpecific = () => {
+  const [step, setStep] = React.useState(0);
+  const [goal, setGoal] = React.useState('');
+  const [howKnow, setHowKnow] = React.useState('');
+  const [posOne, setPosOne] = React.useState('');
+  const [posTwo, setPosTwo] = React.useState('');
+  const [obOne, setObOne] = React.useState('');
+  const [ObTwo, setObTwo] = React.useState('');
+  const [elPosOne, setElPosOne] = React.useState('');
+  const [elPosTwo, setElPosTwo] = React.useState('');
+  const [elObOne, setelObOne] = React.useState('');
+  const [elObTwo, setelObTwo] = React.useState('');
+  return <div></div>;
+};
+
+export default ParentSpecific;
