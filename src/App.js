880f6f7da948d1116b5a10dfced668edab2b3436
John Bede <johnbede2000@gmail.com>
2021-02-28 14:15:00 +0000

    fluid width container for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Container></Container>
     </>
   );
 }
