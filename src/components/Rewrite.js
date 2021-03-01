db67dfe28067cdc31d5d43e63886b541cc495a72
John Bede <johnbede2000@gmail.com>
2021-03-01 16:18:00 +0000

    buttons on third page

--- src/components/Rewrite.js
+++ src/components/Rewrite.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, TextField } from '@material-ui/core';
+import { PrevButton, NextButton } from './Buttons';
 
-const Rewrite = ({ values, handleChange }) => {
+const Rewrite = ({ values, handleChange, step, setStep }) => {
+  const [err, setErr] = React.useState(false);
   return (
     <>
       <Card>
@@ -20,12 +22,23 @@ const Rewrite = ({ values, handleChange }) => {
         id="rewrite"
         label="My improved goal"
         type="text"
+        error={err}
         name="rewrite"
         value={values.rewrite}
         multiline="true"
         variant="filled"
         onChange={(e) => handleChange(e)}
       />
+      <div>
+        <PrevButton step={step} setStep={setStep}></PrevButton>
+        <NextButton
+          field={values.rewrite} // is there shorthand for these?
+          step={step}
+          setStep={setStep}
+          err={err}
+          setErr={setErr}
+        />
+      </div>
     </>
   );
 };
