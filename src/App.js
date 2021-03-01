32aaca303a58913fface8c9c8f38c4ca6fbd9b9f
John Bede <johnbede2000@gmail.com>
2021-03-01 10:46:18 +0000

    import react on app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import ParentSpecific from './components/ParentSpecific';
