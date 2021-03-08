74f7eaf0f01f70c4ca763599a3b77ba676c0c3e7
John Bede <johnbede2000@gmail.com>
2021-03-08 14:16:42 +0000

    changed background color using custom theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { CssBaseline, Container, Box } from '@material-ui/core';
 import ParentSpecific from './components/ParentSpecific';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: '#f6f6f6',
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Container maxWidth="md">
-        <Box py={2.5}>
-          <ParentSpecific />
-        </Box>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box py={3}>
+            <ParentSpecific />
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
