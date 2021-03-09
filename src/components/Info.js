bbb9665d365e9e704b1ac5890f5eb25c7dd80b76
John Bede <johnbede2000@gmail.com>
2021-03-09 16:14:35 +0000

    info component margin

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,7 +5,7 @@ import { Box, Typography } from '@material-ui/core';
 const Info = ({ text }) => {
   return (
     <Box display="flex">
-      <Box flexShrink="0">
+      <Box flexShrink="0" mr={2}>
         <InfoIcon />
       </Box>
       <Typography variant="body2">{text}</Typography>
